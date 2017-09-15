dea8647502d47d528e37c06b7e7724fbb020fcca
tommyeastman1 <tommyeastman1@gmail.com>
2017-09-14 21:40:18 -0300

    create separate index file to retain original index-START. update app.js so audio restarts everytime key is pressed.

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,6 +1,9 @@
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
   if (!audio) return;
+  //every time key is pressed, restart audio file to beginning.
+  //otherwise, js waits until audio clip is done playing to restart.
+  audio.currentTime = 0;
   audio.play();
   //console.log(audio);
 });
